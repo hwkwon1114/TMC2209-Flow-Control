89b5cc0e18281e952bddecbdc853b6e1a957b615
Hyunwoo Kwon <60677560+xdxul123@users.noreply.github.com>
2023-06-06 15:38:08 -0500

    Add files via upload

--- 2209PID.py
+++ 2209PID.py
@@ -11,76 +11,97 @@ from sensirion_driver_adapters.i2c_adapter.i2c_channel import I2cChannel
 from sensirion_i2c_sf06_lf.device import Sf06LfDevice
 from sensirion_i2c_sf06_lf.commands import InvFlowScaleFactors
 
-TUBE_AREA = math.pi * (0.4 * 2.56)**2 #Area of Pipe
-DESIRED_DISPLACEMENT = 0.5 #ml
-CURRENT_CHANGE = 0 #ml
-
 # Constants
-STEP_PIN = 18        # GPIO pin for step signal
-DIR_PIN = 17          # GPIO pin for direction signal
-FLOW_SENSOR_PIN = 17   # GPIO pin for flow sensor input
-PULSES_PER_REV = 256 * 200   # Number of pulses for one full revolution of the motor
+TUBE_AREA = math.pi * (0.4 * 2.56) ** 2  # Area of Pipe
+DESIRED_DISPLACEMENT = 50  # ml
+STEP_PIN = 18  # GPIO pin for step signal
+DIR_PIN = 17  # GPIO pin for direction signal
+FLOW_SENSOR_PIN = 21  # GPIO pin for flow sensor input
+PULSE_PER_STEP = 256
+STEPS_PER_REV = 1000  # 1.8 degree
+CURRENT_CHANGE = 0
 P = 0.1
 I = 0.5
 D = 0.1
 
-import RPi.GPIO as GPIO
 
 class Stepper_Driver(object):
-    def __init__(self, pulsesPerStep, stepsPerRev):
-        self._pinStep = STEP_PIN
-        self._pinDir = DIR_PIN
-        self._pulsesPerStep = pulsesPerStep
-        self._stepsPerRev = stepsPerRev
-        self._pulseCountAbs = 0
-        self._pulseSpeed = -1
-        self._pulseAcceleration = -1
-
+    def __init__(self):
+        self.pinStep = STEP_PIN
+        self.pinDir = DIR_PIN
+        self.pulseSpeed = 500  # Steps per second
+        self.pulseDuration = 1 / self.pulseSpeed
+        self.maxSpeed = 1500
+        self.minSpeed = 0
         GPIO.setwarnings(False)
         GPIO.setmode(GPIO.BCM)
-        GPIO.setup(self._pinStep, GPIO.OUT)
-        GPIO.setup(self._pinDir, GPIO.OUT)
+        GPIO.setup(self.pinStep, GPIO.OUT)
+        GPIO.setup(self.pinDir, GPIO.OUT)
 
+    def setSpeed(self, desired_speed):
+        if desired_speed > self.maxSpeed:
+            desired_speed = self.maxSpeed
+        elif desired_speed < self.maxSpeed:
+            desired_speed = self.minSpeed
+        self.pulseSpeed = desired_speed
+        self.pulseDuration = 1 / self.pulseSpeed
 
-    def setSpeed(self, desired_speed, acceleration_rate):
-        time_to_reach_speed = (desired_speed - self._pulseSpeed) / acceleration_rate
-        steps_to_reach_speed = 0.5 * acceleration_rate * (time_to_reach_speed ** 2)
-        self._pulseSpeedTarget = desired_speed
-        self._pulseAcceleration = acceleration_rate
-        self._pulseCountAbsTarget = self._pulseCountAbs + steps_to_reach_speed
+    def begin(self):
+        try:
+            while DESIRED_DISPLACEMENT > CURRENT_CHANGE:
+                temp = self.pulseDuration
+                GPIO.output(self.pinStep, GPIO.HIGH)
+                time.sleep(temp / 2)
+                GPIO.output(self.pinStep, GPIO.LOW)
+                time.sleep(temp / 2)
+        except KeyboardInterrupt:
+            GPIO.cleanup()
+
 
 class FlowSensor:
     def __init__(self, channel):
         self.device = Sf06LfDevice(channel=channel)
-        self.volume = 0.0
+        self.prev_time = 0
+        self.integrated_error = 0
+        self.timediff = 0
+        self.prev_voldiff = 0
 
     def start_measurement(self):
         self.device.start_h2o_continuous_measurement()
+        self.timediff = time.time()
 
     def stop_measurement(self):
         self.device.stop_continuous_measurement()
 
     def read_measurement(self):
-        flow, temperature, signaling_flags = self.device.read_measurement_data(InvFlowScaleFactors.SLF3S_1300F)
-        self.volume += flow * time. # in ml
-        return flow, temperature, signaling_flags, self.volume
+        global CURRENT_CHANGE
+        flow, _, _ = self.device.read_measurement_data(InvFlowScaleFactors.SLF3C_1300F)
+        currenttime = time.time()
+        self.timediff = currenttime - self.prev_time
+        CURRENT_CHANGE += flow * self.timediff * TUBE_AREA  # in ml
+        self.prev_time = currenttime
 
-# Initialize the device
+
+# Initialize the devices
+stepper_driver = Stepper_Driver()
 flow_sensor = FlowSensor(channel=1)  # replace with your I2C channel
-
 # Start continuous measurement
 flow_sensor.start_measurement()
-
+stepper_driver.begin()
 try:
     while True:
         # Read measurement data
-        flow, temperature, signaling_flags, volume = flow_sensor.read_measurement()
-
+        flow_sensor.read_measurement()
         # Print the current flow, temperature, and total volume
-        print(f"Flow: {flow} ml/min, Temperature: {temperature} °C, Total Volume: {volume} ml")
-
+        error = DESIRED_DISPLACEMENT - CURRENT_CHANGE
+        flow_sensor.integrated_error += error * flow_sensor.timediff
+        derivative = (error - flow_sensor.prev_voldiff) / flow_sensor.timediff
+        speed = P * error + I * flow_sensor.integrated_error + D * derivative
+        stepper_driver.setSpeed(speed)
+        flow_sensor.prev_voldiff = error
         # Wait for 1 second
         time.sleep(1)
 except KeyboardInterrupt:
     # Stop continuous measurement
     flow_sensor.stop_measurement()
+    GPIO.cleanup()
